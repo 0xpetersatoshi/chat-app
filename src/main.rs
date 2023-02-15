f2de24f17b198788211fb0a48a9183de4aa85db1
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2023-02-14 19:11:26 -0700

    feat: add create_user to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use backend::handlers::users::create_user;
+use backend::database::establish_connection;
+
 fn main() {
-    println!("Hello world!")
+    let conn = &mut establish_connection();
+    create_user("user", "user@myemail.com", "password", conn)
 }
