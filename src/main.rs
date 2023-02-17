062f0ec0258ecc95f3ee68c3f355213ab7b7c47c
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2023-02-16 23:18:16 -0700

    refactor: update main.rs to test new handler functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-use backend::handlers::users::create_user;
+use backend::handlers::users::{create_user, get_user_by_email};
 use backend::database::establish_connection;
 
 fn main() {
+    let user_email = "user@myemail.com";
     let conn = &mut establish_connection();
-    create_user("user", "user@myemail.com", "password", conn)
+    create_user("user", user_email, "password", conn);
+    let user = match get_user_by_email(conn, user_email) {
+        Ok(Some(user)) => user,
+        Ok(None) => {
+            panic!("User not found");
+        }
+        Err(e) => {
+            panic!("Error getting user: {}", e);
+        }
+    };
+    println!("User {:?}", user);
 }
