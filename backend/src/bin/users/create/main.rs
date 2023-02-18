ef1eedb2731d62adc0d179afdfc15f61b033eeed
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2023-02-17 20:05:58 -0700

    refactor: reorganize bin structure

--- backend/src/bin/users/create/main.rs
+++ backend/src/bin/users/create/main.rs
@@ -1,10 +1,16 @@
 use backend::database::establish_connection;
-use backend::handlers::users::{create_user, get_user_by_email_or_username};
+use backend::handlers::users::{create_user, get_user_by_email};
+use std::env;
 
 fn main() {
-    let user_email = "user_2@email.com";
-    let username = "some_username";
-    let password = "password123";
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 4 {
+        panic!("Please provide email, username, and password as command line arguments");
+    }
+
+    let user_email = &args[1];
+    let username = &args[2];
+    let password = &args[3];
 
     let conn = &mut establish_connection();
     let new_user = create_user(conn, username, user_email, password);
@@ -12,7 +18,7 @@ fn main() {
         "Created new user {} with ID={}",
         new_user.username, new_user.id
     );
-    let user = match get_user_by_email_or_username(conn, &new_user.email) {
+    let user = match get_user_by_email(conn, &new_user.email) {
         Ok(Some(user)) => user,
         Ok(None) => {
             panic!("User not found");
