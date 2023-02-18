d48194f80b1a1f94f6b73bdca999e6c9c09e594a
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2023-02-17 20:36:09 -0700

    refactor: refactor update-user binary crate

--- backend/src/bin/users/update/main.rs
+++ backend/src/bin/users/update/main.rs
@@ -1,5 +1,6 @@
 use backend::database::establish_connection;
 use backend::handlers::users::{get_user_by_email, get_user_by_username, update_user};
+use backend::models::users::User;
 use std::env;
 
 fn main() {
@@ -13,43 +14,27 @@ fn main() {
     let new_value = &args[3];
 
     let conn = &mut establish_connection();
-    let result: Result<(), String>;
+    let user: User;
 
     if update_field == "email" {
-        let user = match get_user_by_email(conn, old_value) {
+        user = match get_user_by_email(conn, old_value) {
             Ok(Some(u)) => u,
-            Ok(None) => {
-                panic!("No user with username {} was found", old_value);
-            }
-            Err(e) => {
-                panic!("Error: {}", e);
-            }
-        };
-
-        result = match update_user(conn, user.id, None, Some(new_value.clone()), None) {
-            Ok(_) => Ok(()),
-            Err(e) => {
-                panic!("Error: {}", e);
-            }
+            Ok(None) => panic!("No user with username {} was found", old_value),
+            Err(e) => panic!("Error: {}", e),
         };
     } else {
-        let user = match get_user_by_username(conn, old_value) {
+        user = match get_user_by_username(conn, old_value) {
             Ok(Some(u)) => u,
-            Ok(None) => {
-                panic!("No user with username {} was found", old_value);
-            }
-            Err(e) => {
-                panic!("Error: {}", e);
-            }
-        };
-
-        result = match update_user(conn, user.id, Some(new_value.clone()), None, None) {
-            Ok(_) => Ok(()),
-            Err(e) => {
-                panic!("Error: {}", e);
-            }
+            Ok(None) => panic!("No user with username {} was found", old_value),
+            Err(e) => panic!("Error: {}", e),
         };
     }
+
+    let result: Result<(), String> =
+        match update_user(conn, user.id, Some(new_value.clone()), None, None) {
+            Ok(_) => Ok(()),
+            Err(e) => panic!("Error: {}", e),
+        };
 
     match result {
         Ok(_) => println!("User updated successfully"),
