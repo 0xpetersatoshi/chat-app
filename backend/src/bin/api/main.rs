10c993b868a3a27a3afd663e87a36814baa8d856
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2023-02-18 21:34:20 -0700

    feat: add api binary crate

---
+++ backend/src/bin/api/main.rs
@@ -0,0 +1,22 @@
+use actix_web::web::Data;
+use actix_web::{App, HttpServer, web};
+use backend::database::establish_connection;
+use backend::handlers;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let pool = establish_connection();
+    let data = web::Data::new(pool);
+
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(data.clone())
+            .service(handlers::users::get_user_by_id)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
